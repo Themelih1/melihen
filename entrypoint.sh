ea89961f6e68040b1189b9a33cedce6f260fea9a
Themelih1 <melih.yilmaz904@gmail.com>
2025-06-08 15:59:19 +0200

    Add admin auth data and updated entrypoint for loaddata

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,12 @@ set -eux
 
 mkdir -p logs
 
+echo "Running migrations..."
+python manage.py migrate --noinput
+
+echo "Loading admin user data..."
+python manage.py loaddata auth_data.json || true  # dosya yüklüyse hata vermez, devam eder
+
 echo "Running collectstatic..."
 python manage.py collectstatic --noinput
 
